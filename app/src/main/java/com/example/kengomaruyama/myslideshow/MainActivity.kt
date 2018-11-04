01b6ca119ec5eba05d990309664d018d1815718a
klcatha <klcatha0518@gmail.com>
2018-11-04 10:33:40 +0900

    image view set on click listener

--- app/src/main/java/com/example/kengomaruyama/myslideshow/MainActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/myslideshow/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.kengomaruyama.myslideshow
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView.setOnClickListener {
+            it.animate().apply {
+                duration = 3000L
+                rotation(360.0f * 5.0f)
+            }
+        }
     }
 }
